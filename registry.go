2c3e53520310d64417caa39a63107c51cd8813a1
NotFound1911 <465442062@qq.com>
2023-10-06 10:51:43 +0800

    [fix]进一步封装

--- registry.go
+++ registry.go
@@ -12,30 +12,34 @@ type registry struct {
 }
 
 func (r *registry) get(val any) (*model, error) {
-	return r.parseModel(val)
-}
-func (r *registry) parseModel(val any) (*model, error) {
 	typ := reflect.TypeOf(val)
 	m, ok := r.models.Load(typ)
 	if !ok {
-		// 只支持一级指针
-		if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
-			return nil, errs.NewErrPointerOnly(val)
-		}
-		typ = typ.Elem()
-		numField := typ.NumField()
-		fds := make(map[string]*field, numField)
-		for i := 0; i < numField; i++ {
-			fdType := typ.Field(i)
-			fds[fdType.Name] = &field{
-				colName: underscoreName(fdType.Name),
-			}
-		}
-		m = &model{
-			tableName: underscoreName(typ.Name()),
-			fieldMap:  fds,
+		var err error
+		if m, err = r.parseModel(val); err != nil {
+			return nil, err
 		}
 	}
 	r.models.Store(typ, m)
 	return m.(*model), nil
+}
+func (r *registry) parseModel(val any) (*model, error) {
+	typ := reflect.TypeOf(val)
+	// 只支持一级指针
+	if typ.Kind() != reflect.Ptr || typ.Elem().Kind() != reflect.Struct {
+		return nil, errs.NewErrPointerOnly(val)
+	}
+	typ = typ.Elem()
+	numField := typ.NumField()
+	fds := make(map[string]*field, numField)
+	for i := 0; i < numField; i++ {
+		fdType := typ.Field(i)
+		fds[fdType.Name] = &field{
+			colName: underscoreName(fdType.Name),
+		}
+	}
+	return &model{
+		tableName: underscoreName(typ.Name()),
+		fieldMap:  fds,
+	}, nil
 }
